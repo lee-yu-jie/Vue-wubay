f9113c4e2bcf5390d14adabd3f69d693b4aa8611
LI YU JIE <yujie@LIdeMacBook-Air.local>
2023-02-25 15:16:06 +0800

    安裝vite-plugin-pages vite-plugin-vue-layouts

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite";
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,8 @@ export default defineConfig({
       include: [/\.vue$/, /\.vue\?vue/],
       dts: "src/auto-components.js",
     }),
+    Pages(),
+    Layouts(),
   ],
   resolve: {
     alias: {
